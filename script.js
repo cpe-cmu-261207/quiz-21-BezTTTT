f5f80b2c01e4ea38ed6f0fb9e865d099bf07d98b
BezTTTT <Best_1614@hotmail.com>
2022-08-04 14:19:49 +0700

    ชื่อไม่ขึ้น

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
 }
 const imgprofile = document.querySelector("#img-profile");
-const loading = (document.getElementById("div-user-card").style.display =
-  "none");
+
 const btnRandom = document.querySelector("#btn-random");
 
 btnRandom.onclick = async () => {
