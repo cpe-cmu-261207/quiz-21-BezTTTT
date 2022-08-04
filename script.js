f6df8fe30b818c89d8aad6c25cec6f1f8eb7de66
BezTTTT <Best_1614@hotmail.com>
2022-08-04 14:11:46 +0700

    ทำไม่เป็นค้าบ

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
 }
+const imgprofile = document.querySelector("#img-profile");
+const loading = (document.getElementById("div-user-card").style.display =
+  "none");
+const btnRandom = document.querySelector("#btn-random");
+
+btnRandom.onclick = async () => {
+  btnRandom.disabled = false;
+};
